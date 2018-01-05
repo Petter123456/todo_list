df15bcf79dafecce362294f4698436dd150b178a
Petter Fagerlund <petter.fagerlund@gmail.com>
2018-01-05 13:34:15 -0500

    fixed postgres

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'todo_items#index'
   resources :comments
   resources :todo_items #generates get 'path' => controller#action
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
